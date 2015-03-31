67264065c50f906686d7e16da879d9f067531733
Farhan Faruque <farhan.faruque@tu.no>
2015-03-31 16:01:20 +0600

    add paperclip for image

--- bSnapServer/app/models/post.rb
+++ bSnapServer/app/models/post.rb
@@ -4,4 +4,8 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_and_belongs_to_many :categories
 
+
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url =>  ActionController::Base.helpers.asset_path('cam.png')
+
+
 end
--- bSnapServer/db/schema.rb
+++ bSnapServer/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150324100714) do
+ActiveRecord::Schema.define(version: 20150331091845) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace",     limit: 255
@@ -62,12 +62,16 @@ ActiveRecord::Schema.define(version: 20150324100714) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string   "title",       limit: 255
-    t.string   "description", limit: 255
-    t.integer  "location_id", limit: 4
-    t.integer  "user_id",     limit: 4
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
+    t.string   "title",              limit: 255
+    t.string   "description",        limit: 255
+    t.integer  "location_id",        limit: 4
+    t.integer  "user_id",            limit: 4
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.string   "image_file_name",    limit: 255
+    t.string   "image_content_type", limit: 255
+    t.integer  "image_file_size",    limit: 4
+    t.datetime "image_updated_at"
   end
 
   create_table "searches", force: :cascade do |t|
