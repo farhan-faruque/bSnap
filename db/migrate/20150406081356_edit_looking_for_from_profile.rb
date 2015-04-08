cb2cc3de97344809f931301d190251aeda6dc177
Farhan Faruque <russel.cse05@gmail.com>
2015-04-08 11:12:39 +0600

    change column type

--- db/migrate/20150406081356_edit_looking_for_from_profile.rb
+++ db/migrate/20150406081356_edit_looking_for_from_profile.rb
@@ -1,5 +1,7 @@
 class EditLookingForFromProfile < ActiveRecord::Migration
   def change
-    change_column :profiles, :lookingfor, :integer
+    #change_column :profiles, :lookingfor, :integer
+    remove_column :profiles, :lookingfor
+    add_column :profiles, :lookingfor, :integer, default: 0
   end
 end
