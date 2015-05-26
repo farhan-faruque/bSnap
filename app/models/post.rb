f51700e5cbd85ab704d6f522c2096b2c045573a9
Farhan Faruque <russel.cse05@gmail.com>
2015-05-26 12:27:06 +0600

    add user readable urls

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,16 @@
 class Post < ActiveRecord::Base
+  #extend FriendlyId
 
   belongs_to :location
   belongs_to :user
   has_and_belongs_to_many :categories
   has_many :post_attachments
   accepts_nested_attributes_for :post_attachments
+
+  #friendly_id :slug_candidates, use: [:slugged, :finders]
+
+  def to_param
+    "#{id} #{title}".parameterize
+  end
 
 end
