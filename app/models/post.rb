8e6b6fc84beb5e4536f283b055b78443d1c45e6b
Farhan Faruque <russel.cse05@gmail.com>
2015-05-26 13:11:16 +0600

    add friendlyid

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  #extend FriendlyId
+  extend FriendlyId
 
   belongs_to :location
   belongs_to :user
@@ -8,9 +8,12 @@ class Post < ActiveRecord::Base
   accepts_nested_attributes_for :post_attachments
 
   #friendly_id :slug_candidates, use: [:slugged, :finders]
-
-  def to_param
-    "#{id} #{title}".parameterize
+  friendly_id :title, use: :slugged
+  # def to_param
+  #   "#{id} #{title}".parameterize
+  # end
+  def should_generate_new_friendly_id?
+    new_record? || slug.nil? || slug.blank? # you can add more condition here
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150416104359) do
+ActiveRecord::Schema.define(version: 20150526060746) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace",     limit: 255
@@ -85,7 +85,10 @@ ActiveRecord::Schema.define(version: 20150416104359) do
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
     t.string   "image",       limit: 255
+    t.string   "slug",        limit: 255
   end
+
+  add_index "posts", ["slug"], name: "index_posts_on_slug", using: :btree
 
   create_table "profiles", force: :cascade do |t|
     t.integer  "location_id",    limit: 4
